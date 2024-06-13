64ca081f7b87b4c763138b5afa7686bf656f15aa
ZigmasP <zigmas.1@gmail.com>
2024-06-13 21:40:34 +0300

    pridėtas db prisijungimas

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 - Pasileidžiu terminalą.
 - Sukuriu backend "mkdir backend", "cd backend", "npm init -y", įdiegiu projekui reikalingas bibliotekas "npm install ...".
 - Susikuriu index.js ir .env.
-- 
+- Susikuriu hash_password.js ir susikonfiguruoju.
+- Sukuriu uploads ir public katalogus.
+- Uploads bus saugomos nuotraukos.
--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,8 @@ const fs = require('fs');
 const compression = require('compression');
 const brypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { waitForDebugger } = require('inspector');
+const { queue } = require('sharp');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,3 +20,26 @@ const uploadsPath = path.join(__dirname, 'uploads');
 if (!fs.existsSync(uploadsPath)) {
   fs.mkdirSync(uploadsPath);
 }
+
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
+app.use('/uploads', express.static(uploadsPath));
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Duomenų bazės prisijungimai
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  datebase: process.env.DB_NAME,
+  charset: 'utf8mb4',
+  waitForConnections: true,
+  conectionLIimit: 10,
+  queueLimit: 0
+});
+
+app.listen(port, () => {
+  console.log(`Serveris veikia ant ${port}`);
+});
