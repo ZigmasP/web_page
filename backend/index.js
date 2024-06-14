3403017766303fb8eb3b670ddcafe71835c9f100
ZigmasP <zigmas.1@gmail.com>
2024-06-14 07:42:33 +0300

    pridėta middleware

--- backend/index.js
+++ backend/index.js
@@ -94,6 +94,19 @@ app.post('/login', async (req, res) => {
   }
 });
 
+// Middleware funkcija autentiškumui patikrinti
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.status(401).json ({ 'Neteisėta prieiga' });
+
+  jwt.verify(token. process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ error: 'Neteisėta prieiga' });
+    req.user = user;
+    next();
+  });
+};
+
 app.listen(port, () => {
   console.log(`Serveris veikia ant ${port}`);
 });
