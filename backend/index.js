7dd638f6866547302b0fe0d98e9ece3044adf914
ZigmasP <zigmas.1@gmail.com>
2024-06-14 08:21:40 +0300

    pridėta put, get, post, delete

--- backend/index.js
+++ backend/index.js
@@ -97,10 +97,10 @@ app.post('/login', async (req, res) => {
 // Middleware funkcija autentiškumui patikrinti
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-  if (token == null) return res.status(401).json ({ 'Neteisėta prieiga' });
+  const token = authHeader && authHeader.split(' ')[1]
+  if (token == null) return res.status(401).json ({ error: 'Neteisėta prieiga' });
 
-  jwt.verify(token. process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) return res.status(403).json({ error: 'Neteisėta prieiga' });
     req.user = user;
     next();
