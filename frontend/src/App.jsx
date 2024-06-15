40b58cc13e04aac3235180a30b4831992c30b758
ZigmasP <zigmas.1@gmail.com>
2024-06-15 09:36:55 +0300

    Pridėtas header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect, useCallback } from "react";
+import { FaBars, FaTimes, FaReact, FaJs, FaNode, FaBuilding, FaUniversity, FaMapMarkerAlt, FaEnvelope, FaPhone } from "react-icons/fa";
+import logo from "./assets/logo.webp"; 
+import profileImage from "./assets/mano.webp"; // Pakeičiam į tinkamą nuorodą į paveikslėlio failą
+import axios from "axios";
+import "./index.scss";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [works, setWorks] = useState([]);
+
+  const toggleMenu = useCallback(() => {
+    setIsMenuOpen(prev => !prev);
+  }, []);
+
+  const closeMenu = useCallback(() => {
+    setIsMenuOpen(false);
+  }, []);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:3000/works')
+    .then((response) => {
+      setWorks(response.data);
+    })
+    .catch((error) => {
+      console.error('Klaida gaunant duomenis:', error);
+    });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div>
+      <header>
+        <div className="logo-container">
+          <img src={logo} alt="zigmasebdev logo" className="logo" loading="lazy" />
+        </div>
+        <nav className={isMenuOpen ? "open" : ""} aria-label="Main Navigation">
+          <ul>
+            <li>
+              <a href="#home" onClick={closeMenu}>Pagrindinis</a>
+            </li>
+            <li>
+              <a href="#about" onClick={closeMenu}>Apie mane</a>
+            </li>
+            <li>
+              <a href="#works" onClick={closeMenu}>Mano darbai</a>
+            </li>
+            <li>
+              <a href="#contacts" onClick={closeMenu}>Kontaktai</a>
+            </li>
+          </ul>
+        </nav>
+        <button className="menu-toggle" onClick={toggleMenu} aria-label="Toggle Menu">
+          {isMenuOpen ? <FaTimes /> : <FaBars />}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      </header>
 
-export default App
+      
+    </div>
+  )
+};
+
+export default App;
