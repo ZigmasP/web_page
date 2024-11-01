a5ae7c8bdbb0b30c6343bb715c312006064bb872
Zigmas Petrauskas <zigmas.1@gmail.com>
2024-11-01 09:58:16 +0200

    sukonfiguruota dėl githup pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { terser } from 'rollup-plugin-terser'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { terser } from 'rollup-plugin-terser';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/web_page/', // Nustatykite savo GitHub Pages URL bazę
   plugins: [react(), terser()],
   build: {
     rollupOptions: {
@@ -14,4 +15,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
